5922df089345f26a696f04d15f6de787c9bcedb1
Saber Modirian <sabermodirian@gmail.com>
2026-09-09 18:51:34 +0330

    6)ساخت صفحه ی لاگین و تست موفق آن
    Signed-off-by: Saber Modirian <sabermodirian@gmail.com>

--- config/settings.py
+++ config/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))], # برای پیدا کردن هر تمپلیتی در این آدرس هم بگرد
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
