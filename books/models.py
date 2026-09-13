6f397b2572a3f069d52c6cf2b4326dc590a3a549
Saber Modirian <sabermodirian@gmail.com>
2026-09-13 14:32:06 +0330

    17)تغییرات کلاس book   پنل admin.py کردن آن و migrate کردن
    Signed-off-by: Saber Modirian <sabermodirian@gmail.com>

--- books/models.py
+++ books/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-	title = models.CharField(max_length = 200)
-	author = models.CharField(max_length = 200)
-	description = models.TextField()
-	price = models.DecimalField(decimal_places = 3 , max_digits = 7)
+	title = models.CharField(max_length = 200 , verbose_name = 'Title')
+	author = models.CharField(max_length = 200 , verbose_name = 'Author')
+	description = models.TextField(verbose_name = 'Description')
+	price = models.DecimalField(decimal_places = 3 , max_digits = 7 , verbose_name = 'Price')
 	#decimal_places : تعداداعشار , max_digits : تعدادکل ارقام
 	def __str__(self):
 		return self.title
