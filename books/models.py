0cdbd41ac922a8a80e43abb8e02c03f2b768e842
Saber Modirian <sabermodirian@gmail.com>
2026-09-13 14:16:12 +0330

    16)طراحی و پیاده سازی فیلدهای کلاس book و migrate کردن آن
    Signed-off-by: Saber Modirian <sabermodirian@gmail.com>

--- books/models.py
+++ books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+	title = models.CharField(max_length = 200)
+	author = models.CharField(max_length = 200)
+	description = models.TextField()
+	price = models.DecimalField(decimal_places = 3 , max_digits = 7)
+	#decimal_places : تعداداعشار , max_digits : تعدادکل ارقام
+	def __str__(self):
+		return self.title
