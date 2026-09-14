4bbec7b5dd907c5f9d5c4678176356ae7d9f86ed
Saber Modirian <sabermodirian@gmail.com>
2026-09-14 13:21:17 +0330

    22)ایجاد ویرایش برای کتابها : BookUpdateViewو book_update.html
    Signed-off-by: Saber Modirian <sabermodirian@gmail.com>

--- books/urls.py
+++ books/urls.py
@@ -1,12 +1,13 @@
 
 from django.urls import path
 
-from books.views import BookDetailsView , BooklistView , BookCreateView
+from books.views import BookDetailsView , BookUpdateView , BooklistView , BookCreateView
 
 app_name = 'books'
 
 urlpatterns = [
 	path('', BooklistView.as_view() , name = 'book_list'),
 	path('<int:pk>/' , BookDetailsView.as_view()  , name = 'book_details' ),
-	path('create/' , BookCreateView.as_view() , name = 'book_create')
+	path('create/' , BookCreateView.as_view() , name = 'book_create'),
+	path('<int:pk>/edit/' , BookUpdateView.as_view() , name = 'book_update'),
 ]
