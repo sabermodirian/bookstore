84e0b62ffef6816b12999ea0189135f564942eda
Saber Modirian <sabermodirian@gmail.com>
2026-09-21 13:22:08 +0330

    48) ایجاد کلاس (table)  کامنت گذاریComment  به مدل ادمین و رجیستر کردن آن در پنل ادمین

--- books/admin.py
+++ books/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book , Comment
 # Register your models here.
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
 	model = Book
 	
+@@admin.register(Comment)
+class ModelNameAdmin(admin.ModelAdmin):
+	model = Comment
  
+	
